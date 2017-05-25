d019285ee592f1cd3189c4397640148349fd1aaa
rfdouro <rfdouro@gmail.com>
2017-05-25 15:21:09 -0300

    01

---
+++ MVC02/Controllers/PessoaController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC02.Models;
+
+namespace MVC02.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private PContexto db = new PContexto();
+
+        //
+        // GET: /Pessoa/
+
+        public ActionResult Index()
+        {
+            return View(db.pessoas.ToList());
+        }
+
+        //
+        // GET: /Pessoa/Details/5
+
+        public ActionResult Details(long id = 0)
+        {
+            Pessoa pessoa = db.pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        //
+        // GET: /Pessoa/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Pessoa/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pessoas.Add(pessoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoa);
+        }
+
+        //
+        // GET: /Pessoa/Edit/5
+
+        public ActionResult Edit(long id = 0)
+        {
+            Pessoa pessoa = db.pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        //
+        // POST: /Pessoa/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pessoa);
+        }
+
+        //
+        // GET: /Pessoa/Delete/5
+
+        public ActionResult Delete(long id = 0)
+        {
+            Pessoa pessoa = db.pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        //
+        // POST: /Pessoa/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Pessoa pessoa = db.pessoas.Find(id);
+            db.pessoas.Remove(pessoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC02/Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC02.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public long id { get; set; }
+        public string nome { get; set; }
+    }
+
+    public class PContexto : DbContext
+    {
+        public PContexto()
+            : base()
+        {
+
+        }
+
+        public DbSet<Pessoa> pessoas { get; set; }
+    }
+}
